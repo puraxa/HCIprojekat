c383f08ffbf4bfcc23c1eaf7c8f808ba84d3cf6d
Edin Purivatra <edinpurivatra@gmail.com>
2021-01-15 11:19:24 +0100

    v 0.0.1

--- src/app/rezervacijatermina/rezervacijatermina.component.ts
+++ src/app/rezervacijatermina/rezervacijatermina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rezervacijatermina',
@@ -11,5 +12,39 @@ export class RezervacijaterminaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  date;
+  dateBool:boolean = false;
   termini:Array<number> = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+  tempRezervisani:Array<number> = [];
+  rezervisaniTermini:Array<number>=[];
+  isDate = () => {
+    return this.dateBool;
+  }
+  dodajRezervisani = (i:number):void => {
+    if(this.rezervisaniTermini.indexOf(i)>-1){
+      alert("Termin rezervisan");
+      return;
+    }
+    console.log(this.date);
+    this.tempRezervisani.push(i);
+  }
+  isTempRezervisan = (i:number):boolean => {
+    return this.tempRezervisani.indexOf(i) < 0 ? false : true;
+  }
+  isRezervisan = (i:number):boolean =>{
+    return this.rezervisaniTermini.indexOf(i) < 0 ? false : true;
+  }
+  rezervisi = ():void =>{
+    if(this.date==undefined){
+      this.dateBool = true;
+      alert("Datum nije odabran");
+      return;
+    }
+    for (let i = 0; i < this.tempRezervisani.length; i++) {
+      const element = this.tempRezervisani[i];
+      if(this.rezervisaniTermini.indexOf(element) < 0) this.rezervisaniTermini.push(element);
+      
+    }
+    alert("Uspjesno rezervisani termini");  
+  }
 }
